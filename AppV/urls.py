d47649c60c488112b33c320c3be3abeb265894c8
Virginia Vega <viroviro38@gmail.com>
2024-12-11 16:58:06 -0300

    Ultimas modificaciones al codigo antes de realizar la carga de imagenes a cada blog

--- AppV/urls.py
+++ AppV/urls.py
@@ -3,16 +3,12 @@ from AppV import views
 from AppV import views_vistas
 from django.contrib.auth.views import LogoutView
 
-#PARA LAS IMAGENES
-# from django.conf import settings
-# from django.conf.urls.static import static
-
 
 urlpatterns = [
 path('pages/', views.home, name="inicio"),
 path('about/', views.about, name="about"),
 path('', views_vistas.login_request, name="login"),
-path('register/', views.register, name='register'),
+path('singup/', views.register, name='register'),
 path('logout/', LogoutView.as_view(template_name='appv/logout.html'), name='logout'),
 path('edit/', views.editar_perfil, name="editar"),
 ]
@@ -27,7 +23,3 @@ urls_vista_clases=[
 
 urlpatterns += urls_vista_clases
 
-
-# Servir archivos media en desarrollo PARA LAS IMAGENES
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
